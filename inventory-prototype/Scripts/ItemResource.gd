9e6aa3aeb7f1b6696230d58ae5e4e2472033b2ac
briggz-21 <samcbriggs02@gmail.com>
2026-02-01 17:03:28 +1100

    item resource tres files and quick art

---
+++ inventory-prototype/Scripts/ItemResource.gd
@@ -0,0 +1,18 @@
+class_name ItemResource extends Resource
+
+## Resource for items, specifically in relation to grid based inventory
+## Thusly information such as the amount of HP restored by consumable or damage dealt by weapon will be abstracted
+
+enum ITEM_TYPE
+{
+	CONSUMABLE,
+	EQUIPPABLE,
+	MISC
+}
+
+@export var item_name: String
+@export var item_description: String
+@export var item_type: ITEM_TYPE
+@export var grid_width: int
+@export var grid_height: int
+@export var ability_icon : Texture2D
